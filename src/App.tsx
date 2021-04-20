52153c813c62c41811291bf4fd592376026d8925
Piotr 'Ugz' Bielski <pb93infa@gmail.com>
2021-04-20 22:50:31 +0200

    add and load settings from json file

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import './App.css';
+import settingsJson from "./data/settings.json";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
+    <div className="App" style={{textAlign: 'left'}}>
         <div>
-          <p>In this place you can see values from custom enviroment variables</p>
+          <h1>In this place you can see values from custom enviroment variables</h1>
           <p>For set this value you can set this values before run node server or create ".env" file</p>
           <p>REACT_APP_NAME:</p>
           {process.env.REACT_APP_NAME? (
@@ -20,8 +20,9 @@ function App() {
           ) : (
             <div style={{color: "red"}}>NONE</div>
           )}
+          <h1>It's place where you can see value of settings from json file</h1>
+          <p>GitHub address: <a href={settingsJson.GITHUB_ADDRESS}>{settingsJson.GITHUB_ADDRESS}</a></p>
         </div>
-      </header>
     </div>
   );
 }
